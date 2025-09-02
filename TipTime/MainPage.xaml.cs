fa70609551e0cddcd3d52a6f2f6a60065f147e9f
Lschanhi <lschanhi@outlook.com>
2025-09-01 22:34:21 -0300

    Aula 06 - Criação de uma Interface para calcular Gorjeta

---
+++ TipTime/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿namespace TipTime
+{
+    public partial class MainPage : ContentPage
+    {
+        private double valorTemp;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ValorCompra_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (double.TryParse(ValorCompra.Text, out double valor))
+            {
+                valorTemp = valor;
+            }
+            else
+            {
+                valorTemp = 0;
+            }
+
+
+
+        }
+
+        private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 15;
+            double total = valorTemp * 0.15;
+            valorGorjetaLabel.Text = $"Valor da gorjeta: R$ {total:F2}";
+          
+        }
+
+        private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
+        {
+            PorcentagemSlider.Value = 20;
+            double total = valorTemp * 0.20;
+            valorGorjetaLabel.Text = $"Valor da gorjeta: R$ {total:F2}";
+        }
+
+        private void ArredondarCimaBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ArredondarBaixoBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double valor = PorcentagemSlider.Value;
+            
+           
+        }
+
+     
+    }
+
+}
