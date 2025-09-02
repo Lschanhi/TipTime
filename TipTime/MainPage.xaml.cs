a4980fe2fe9e27fbf3074012f633b3954bba6259
Lschanhi <lschanhi@outlook.com>
2025-09-02 01:55:36 -0300

    Finalização dos Métodos de 15 e 20% e Terminei o Método do slider

--- TipTime/MainPage.xaml.cs
+++ TipTime/MainPage.xaml.cs
@@ -28,15 +28,19 @@
         {
             PorcentagemSlider.Value = 15;
             double total = valorTemp * 0.15;
+            double soma = valorTemp + total;
             valorGorjetaLabel.Text = $"Valor da gorjeta: R$ {total:F2}";
-          
+            valorTotalLabel.Text = $"Valor da gorjeta: R$ {soma:F2}";
+
         }
 
         private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
         {
             PorcentagemSlider.Value = 20;
             double total = valorTemp * 0.20;
+            double soma = valorTemp + total;
             valorGorjetaLabel.Text = $"Valor da gorjeta: R$ {total:F2}";
+            valorTotalLabel.Text = $"Valor Total da Comanda: R$ {soma:F2}";
         }
 
         private void ArredondarCimaBtn_Clicked(object sender, EventArgs e)
@@ -52,8 +56,11 @@
         private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             double valor = PorcentagemSlider.Value;
-            
-           
+            double total = valorTemp * (valor / 100);
+            double soma = valorTemp + total;
+            valorGorjetaLabel.Text = $"Valor Total da Comanda: R$ {total:F2}";
+            valorTotalLabel.Text = $"Valor Total da Comanda: R$ {soma:F2}";
+            valorSliderlabel.Text = $"{valor:F2}%";
         }
 
      
