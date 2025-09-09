a6b34f499c4646c907a9438d6daaa468b4e5e5f6
Lschanhi <lschanhi@outlook.com>
2025-09-08 22:28:33 -0300

    alteraçoes de alguns metodos

--- TipTime/MainPage.xaml.cs
+++ TipTime/MainPage.xaml.cs
@@ -7,9 +7,10 @@
         public MainPage()
         {
             InitializeComponent();
+            
         }
 
-        private void ValorCompra_TextChanged(object sender, TextChangedEventArgs e)
+       private void ValorCompra_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (double.TryParse(ValorCompra.Text, out double valor))
             {
@@ -19,48 +20,71 @@
             {
                 valorTemp = 0;
             }
-
+       
 
 
         }
+       
+
 
         private void Porcentagem15Btn_Clicked(object sender, EventArgs e)
         {
+              
             PorcentagemSlider.Value = 15;
             double total = valorTemp * 0.15;
             double soma = valorTemp + total;
-            valorGorjetaLabel.Text = $"Valor da gorjeta: R$ {total:F2}";
-            valorTotalLabel.Text = $"Valor da gorjeta: R$ {soma:F2}";
+            valorGorjetaLabel.Text = $"Valor da gorjeta:{total:C2}";
+            valorTotalLabel.Text = $"Valor da gorjeta:{soma:C2}";
 
         }
 
         private void Porcentagem20Btn_Clicked(object sender, EventArgs e)
         {
+            
             PorcentagemSlider.Value = 20;
             double total = valorTemp * 0.20;
             double soma = valorTemp + total;
-            valorGorjetaLabel.Text = $"Valor da gorjeta: R$ {total:F2}";
-            valorTotalLabel.Text = $"Valor Total da Comanda: R$ {soma:F2}";
+            valorGorjetaLabel.Text = $"Valor da gorjeta:{total:C2}";
+            valorTotalLabel.Text = $"Valor Total da Comanda:{soma:C2}";
         }
 
         private void ArredondarCimaBtn_Clicked(object sender, EventArgs e)
-        {
+        {   
+            
+            double arredondadoParaCima = Math.Ceiling(PorcentagemSlider.Value);
+            double total = valorTemp * arredondadoParaCima;
+            double soma = total + valorTemp;
+            valorGorjetaLabel.Text = $"Valor da gorjeta:{total:C2}";
+            valorTotalLabel.Text = $"Valor Total da Comanda:{soma:C2}";
+            
 
         }
 
         private void ArredondarBaixoBtn_Clicked(object sender, EventArgs e)
         {
+            
+               double valor = Convert.ToDouble(valorTotalLabel.Text);
+                double baixo = (valor/5)*5;
+                valorTotalLabel.Text = baixo.ToString("c");
 
         }
 
         private void PorcentagemSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            double valor = PorcentagemSlider.Value;
-            double total = valorTemp * (valor / 100);
-            double soma = valorTemp + total;
-            valorGorjetaLabel.Text = $"Valor Total da Comanda: R$ {total:F2}";
-            valorTotalLabel.Text = $"Valor Total da Comanda: R$ {soma:F2}";
-            valorSliderlabel.Text = $"{valor:F2}%";
+            try {
+
+               
+                double valor = PorcentagemSlider.Value;
+                double total = valorTemp * (valor / 100);
+                double soma = valorTemp + total;
+                valorGorjetaLabel.Text = $"Valor Total da Comanda: {total:C2}";
+                valorTotalLabel.Text = $"Valor Total da Comanda: {soma:C2}";
+                valorSliderLabel.Text = $"{valor:F2}";
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
      
